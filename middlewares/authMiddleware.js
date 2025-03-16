b294f2c1320b6ed031c4c5c34316e335f63fa969
Rutuja1923 <rutujajirage19@gmail.com>
2025-03-16 17:39:08 +0530

    authenticate user & restrict

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,67 @@
+const { getUser } = require("../services/auth");
+
+//check if user is authenticated
+async function checkForAuthentication(req, res, next) {
+    try {
+        const tokenCookie = req.cookies?.token;
+        req.user = null;
+
+        if (tokenCookie) {
+            const user = await getUser(tokenCookie);
+            if (user) {
+                req.user = user;
+            }
+        }
+
+        console.log("User in checkForAuthentication:", req.user);
+
+        if (!req.user) {
+            return res.status(401).json(
+                { 
+                    status: "Error",
+                    message: "Not authorized. Please log in.", 
+                }
+            );
+        }
+
+        next();
+    } 
+    catch (error) {
+        return res.status(500).json(
+            { 
+                status: "Server Error",
+                message: "Error in authentication check", 
+                error: error.message 
+            }
+        );
+    }
+}
+
+//restrict access based on roles
+function restrictTo(roles) {
+    return function (req, res, next) {
+        console.log("User in restrictTo:", req.user);
+
+        if (!req.user) {
+            return res.status(401).json(
+                { 
+                    status: "Error",
+                    message: "Unauthorized access. Please log in.", 
+                }
+            );
+        }
+
+        if (!roles.includes(req.user.role)) {
+            return res.status(403).json(
+                { 
+                    status: "Forbidden",
+                    message: "You do not have permission to access this resource.", 
+                }
+            );
+        }
+
+        next();
+    };
+}
+
+module.exports = { checkForAuthentication, restrictTo };
