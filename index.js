ab45e57952210bb2fcbac48e24c9b6b900c97634
Rutuja1923 <rutujajirage19@gmail.com>
2025-03-18 10:53:01 +0530

    Registering task routes:

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const dotenv = require("dotenv");
 dotenv.config();
 const path = require('path');
 const cookieParser = require('cookie-parser');
+
+//Routes
 const authRoutes = require("./routes/authRoutes");
+const taskRoutes = require("./routes/taskRoutes");
 
 const PORT = 3000;
 const app = express();
@@ -17,11 +20,14 @@ app.set('views', path.resolve('./views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+//middlewares
 app.use(express.json());
 app.use(express.urlencoded( {extended: false} ));
 app.use(cookieParser());
 
+//route register
 app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server started at port ${PORT}`);
