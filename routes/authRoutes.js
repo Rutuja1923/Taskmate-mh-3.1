0e42c0008425914435bebeb2887a63e7927bded7
Rutuja1923 <rutujajirage19@gmail.com>
2025-03-18 16:37:11 +0530

    Removed logout route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,9 @@
 const express = require("express");
-const { handleUserSignup, handleUserLogin, handleUserLogout } = require("../controllers/authController");
-const { checkForAuthentication} = require("../middlewares/authMiddleware");
+const { handleUserSignup , handleUserLogin } = require("../controllers/authController");
 
 const router = express.Router();
 
 router.post("/signup", handleUserSignup);
 router.post("/login", handleUserLogin);
-router.post("/logout",checkForAuthentication, handleUserLogout);
 
 module.exports = router;
