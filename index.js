10802c95e266a99d516e49363b82ad1a58194d14
Rutuja1923 <rutujajirage19@gmail.com>
2025-03-18 16:38:57 +0530

    Handling new route code

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const dotenv = require("dotenv");
 dotenv.config();
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const {checkForAuthentication} = require('./middlewares/authMiddleware')
 
 //Routes
 const authRoutes = require("./routes/authRoutes");
 const taskRoutes = require("./routes/taskRoutes");
 const adminRoutes = require("./routes/adminRoutes");
+const homeRoutes = require("./routes/staticRoutes");
 
 const PORT = 3000;
 const app = express();
@@ -25,11 +27,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded( {extended: false} ));
 app.use(cookieParser());
+app.use(checkForAuthentication);
 
 //route register
-app.use("/api/auth", authRoutes);
-app.use("/api/tasks", taskRoutes);
-app.use("/api/admin", adminRoutes);
+// app.use("/api/tasks", taskRoutes);
+// app.use("/api/admin", adminRoutes);
+app.use("/", homeRoutes);
+app.use("/user", authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server started at port ${PORT}`);
