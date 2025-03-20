5e66a339c4ceb5156448ed80bf1e1c14941076db
Rutuja1923 <rutujajirage19@gmail.com>
2025-03-20 19:34:31 +0530

    removed DELETE user route

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const { checkForAuthentication, restrictTo } = require("../middlewares/authMiddleware");
-const { handleGetAllTasks, handleGetAllUsers, handleRemoveUser } = require('../controllers/adminController');
+const { handleGetAllTasks, handleGetAllUsers } = require('../controllers/adminController');
 
 const router = express.Router();
 
+router.get("/", checkForAuthentication, restrictTo(["admin"]), (req, res) => {
+    res.render("adminHome", { user: req.user });
+});
+
 router.get("/tasks", checkForAuthentication, restrictTo(["admin"]), handleGetAllTasks);
 router.get("/users", checkForAuthentication, restrictTo(["admin"]), handleGetAllUsers);
-router.delete("/users/:id", checkForAuthentication, restrictTo(["admin"]), handleRemoveUser);
 
 module.exports = router;
